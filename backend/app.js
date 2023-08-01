bd700181b5516f7653ace46fe1cb3cf3ecd849e6
Mayank Agrawal <105542987+mayankagrawal328@users.noreply.github.com>
2023-08-01 19:58:33 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,75 @@
+const express = require("express")
+const collection = require("./mongo")
+const cors = require("cors")
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+
+
+app.get("/",cors(),(req,res)=>{
+
+})
+
+
+app.post("/",async(req,res)=>{
+    const{email,password}=req.body
+
+    try{
+        const check=await collection.findOne({email:email})
+        if(check&&check.password==password){
+            res.json("exist")
+        }
+        else if(check&&check.password!=password){
+            res.json("password")
+        }
+        else{
+            res.json("notexist")
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
+})
+
+
+
+app.post("/signup",async(req,res)=>{
+    const{email,password}=req.body
+
+    const data={
+        email:email,
+        password:password,
+        x:""
+    }
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+            await collection.insertMany([data])
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
+})
+
+
+
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT,()=>{
+    console.log("port connected");
+})
+
+
